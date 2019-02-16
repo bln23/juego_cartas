91b3d59e25e82e58ff0dd304d4684002d93fbe79
Belén <be23mg@gmail.com>
2019-02-16 17:41:55 +0100

    colocar ventana en el centro

--- src/trivial/ui/TrivialFrame.java
+++ src/trivial/ui/TrivialFrame.java
@@ -6,12 +6,17 @@ import java.awt.*;
 public class TrivialFrame extends JFrame {
 
     public TrivialFrame() {
-        setSize(new Dimension(800, 800));
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        getContentPane().add(new Label("Hola"));
-        setResizable(true);
-        setTitle("Trivial");
+        getContentPane().add(new Label("Trivial"));
+        setResizable(true); //redimensionar
+        setTitle("Trivial"); //nombre ventana
         setVisible(true);
+        Toolkit myWindows = Toolkit.getDefaultToolkit();
+        Dimension sizeWindow = myWindows.getScreenSize();
+        int heightWindow = sizeWindow.height;
+        int witdthWindow = sizeWindow.width;
+        setSize(witdthWindow/2, heightWindow/2);
+        setLocation(witdthWindow/4, heightWindow/4);
     }
 
     public void showCardPanel() {
